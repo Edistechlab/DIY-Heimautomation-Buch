90943c8c4f099b0b1b41c386159d7fbe25234a26
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2020-08-29 13:43:53 +0200

    Create DHT22_V1.0.ino

---
+++ Sensoren/Temperatur Luftfeuchte Sensoren/DHT22_V1.0/DHT22_V1.0.ino
@@ -0,0 +1,47 @@
+/*
+Project:  DHT22 Temperatur und Luftfeuchte Sensor
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 29.08.2020
+Version:  V1.0
+IDE:      Arduino IDE 1.8.13
+ 
+Required Board (Tools -> Board -> Boards Manager...)
+ - Board: esp8266 by ESP8266 Community   V2.7.4 
+ - Board: esp32   by Espressif Systems   V1.0.2
+
+Required libraries (sketch -> include library -> manage libraries)
+ - Adafruit_DHT sensor library V1.3.10 
+ - Adafruit Unified Sensor Library V1.1.4
+
+Wirering for the DHT22 Sensor:
+DHT22       ESP32       ESP8266     UNO         Mega
+VCC         3.3V        3.3V        3.3V        3.3V      
+GND         GND         GND         GND         GND
+OUT         22          D1/5        A5          21
+*/
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN 5        //Pin an dem das OUT Signal angeschlossen ist.
+float temp = 0.00;
+float hum = 0.00;
+DHT_Unified dht(DHTPIN, DHT22);  
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  sensors_event_t event;
+  dht.humidity().getEvent(&event);    //Luftfeuchte messen und ausgeben
+  hum = event.relative_humidity;
+  Serial.print("Humidity: ");
+  Serial.print( String(hum));
+  dht.temperature().getEvent(&event); //Temperatur messen und ausgeben
+  temp = event.temperature;
+  Serial.print("\tTemperature: ");
+  Serial.println( String(temp));
+  delay(2000);    // zwei Sekunden warten, DHT22 Sensor Antwortzeit ist 2 Sek.
+}
