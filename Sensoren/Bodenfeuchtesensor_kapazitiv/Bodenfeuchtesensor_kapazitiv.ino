282daf0decf9e85f1d75d860564b05f9c4e2529a
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2021-05-14 10:48:58 +0200

    Create Bodenfeuchtesensor_kapazitiv.ino

---
+++ Sensoren/Bodenfeuchtesensor_kapazitiv/Bodenfeuchtesensor_kapazitiv.ino
@@ -0,0 +1,38 @@
+/*
+Project:  Capacitive soil moist Sensor
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 13.05.2021
+Version:  V1.0
+IDE:      Arduino IDE 1.8.13
+ 
+Required Board (Tools -> Board -> Boards Manager...)
+ - Board: esp8266 by ESP8266 Community   V2.7.4 
+ - Board: esp32   by Espressif Systems   V1.0.4
+
+Wirering for the Module:
+Modul       ESP32       ESP8266     UNO         Mega
++           3.3V        3.3V        5V          5V      
+-           GND         GND         GND         GND
+A           VP/A0/36    A0          A0          A0
+*/
+
+const int soilMoistPin = A0;
+const int soilMoistLevelLow = 844;    //Dieser Wert soll von euch entsprechend angepasst werden
+const int soilMoistLevelHigh = 377;   //Dieser Wert soll von euch entsprechend angepasst werden
+
+void setup() {
+  Serial.begin(115200);  
+  pinMode(soilMoistPin, INPUT);
+}
+
+void loop() {
+  int soilMoist = analogRead(soilMoistPin);
+  Serial.print("Analog Value: ");
+  Serial.print(soilMoist);
+  // Auswertung der Bodenfeuchtigkeit in Prozent
+  int percentSoilMoist = map(soilMoist, soilMoistLevelHigh, soilMoistLevelLow, 100, 0);
+  Serial.print("\t"); 
+  Serial.print(percentSoilMoist);
+  Serial.println(" %");
+  delay(2000);
+}
