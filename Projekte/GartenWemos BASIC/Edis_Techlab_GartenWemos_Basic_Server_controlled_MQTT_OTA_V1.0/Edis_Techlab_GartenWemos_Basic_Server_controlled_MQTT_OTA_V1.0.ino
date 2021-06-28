3dd2fecd8e0570c0f11a2ebf926cba550c15abd3
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2021-06-28 18:09:23 +0200

    Update Edis_Techlab_GartenWemos_Basic_Server_controlled_MQTT_OTA_V1.0.ino

--- Projekte/GartenWemos BASIC/Edis_Techlab_GartenWemos_Basic_Server_controlled_MQTT_OTA_V1.0/Edis_Techlab_GartenWemos_Basic_Server_controlled_MQTT_OTA_V1.0.ino
+++ Projekte/GartenWemos BASIC/Edis_Techlab_GartenWemos_Basic_Server_controlled_MQTT_OTA_V1.0/Edis_Techlab_GartenWemos_Basic_Server_controlled_MQTT_OTA_V1.0.ino
@@ -1,5 +1,5 @@
 /*
-Project:  Edis Techlab Garduino Basic with MQTT controlled from a Server
+Project:  Edis Techlab Garten-Wemos Basic with MQTT controlled from a Server
 Author:   Thomas Edlinger for www.edistechlab.com
 Date:     Created 20.06.2021
 Version:  V1.0
