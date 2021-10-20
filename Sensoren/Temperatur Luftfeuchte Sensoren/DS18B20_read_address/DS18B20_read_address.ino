9a003c0d4f15659bf5427bb8c2d9dcdee454291a
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2021-10-20 16:51:41 +0200

    Update DS18B20_read_address.ino

--- Sensoren/Temperatur Luftfeuchte Sensoren/DS18B20_read_address/DS18B20_read_address.ino
+++ Sensoren/Temperatur Luftfeuchte Sensoren/DS18B20_read_address/DS18B20_read_address.ino
@@ -45,9 +45,12 @@ void loop(){
   delay(5000);
 }
 
-void printAddress(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++){
-    if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
+void printAddress(DeviceAddress deviceAddress){
+  for (uint8_t i = 0; i < 8; i++) {
+    Serial.print("0x");
+    if (deviceAddress[i] < 0x10) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+    if (i < 7) Serial.print(", ");
   }
+  Serial.println("");
 }
