7157a4619e0a81ea8b01fb4850fce9c323037d7e
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2021-11-26 06:41:10 +0100

    Create Reed-Switch_MQTT_OTA_V1.0.ino.ino

---
+++ Sensoren/Reed-Switch_MQTT_OTA_V1.0.ino/Reed-Switch_MQTT_OTA_V1.0.ino.ino
@@ -0,0 +1,138 @@
+/*
+Project:  Reed Switch with MQTT und OTA update
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 26.11.2021
+Version:  V1.0
+IDE:      Arduino IDE 1.8.16
+ 
+Required Board (Tools -> Board -> Boards Manager...)
+ - Board: esp8266 by ESP8266 Community   V3.0.2 
+ - Board: esp32   by Espressif Systems   V1.0.6
+
+Required libraries (sketch -> include library -> manage libraries)
+ - PubSubClient by Nick ‘O Leary V2.8.0
+ - ArduinoOTA by Juraj Andrassy V1.0.5          
+*/
+
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+//#include <WiFi.h>  // ESP32 only
+
+#define wifi_ssid "Your_SSID"
+#define wifi_password "Your_Password"
+#define mqtt_server "MQTT_Server_IP"
+#define mqtt_user "MQTT_username"         
+#define mqtt_password "MQTT_PW"
+#define ESPHostname "Reed-Switch01"
+String clientId = "Reed-Switch01-"; 
+
+#define status_topic "reed_status_01"
+#define intopic "intopic"
+
+const int reedPin = 5;  // D1 - Wemos D1 mini
+bool switchState;
+bool reedStatusOpen = true;
+const int sensorTakt = 1000; //alle 1 Sekunden wird der Sensor ausgelesen
+long lastMsg = 0;
+char msg[50];
+
+WiFiClient espClient;  
+PubSubClient client(espClient);  
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  ArduinoOTA.setHostname(ESPHostname);
+  // ArduinoOTA.setPassword("admin");
+  ArduinoOTA.begin();
+  client.setServer(mqtt_server, 1883); 
+  client.setCallback(callback); 
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  ArduinoOTA.handle(); 
+  long now = millis();
+  if (now - lastMsg > sensorTakt) {   
+    lastMsg = now;
+    readReedSwitch();
+  }
+}
+
+void readReedSwitch() {
+  switchState = digitalRead(reedPin);
+  if (switchState == LOW) {
+    if (reedStatusOpen == true) {
+      Serial.println("Reed Switch is Closed.");
+      client.publish(status_topic, "HIGH");
+      reedStatusOpen = false;
+    }
+  } 
+  else {
+    if (reedStatusOpen == false) {
+      Serial.println("Reed Switch is Open.");
+      client.publish(status_topic, "LOW");
+      reedStatusOpen = true;
+    }
+  } 
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}  
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == intopic) {
+    
+  }
+}
+
+ void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(status_topic, ESPHostname);
+      // ... and resubscribe
+      client.subscribe(intopic);
+    } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+        // Wait 5 seconds before retrying
+        delay(5000);
+      }
+   }
+}
