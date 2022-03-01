8b220bbb4463bcac9ab72ddf9bc2c1faa0394de6
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2022-03-01 07:09:18 +0100

    Create Poti_NodeMCU_JSON_Generieren.ino

---
+++ JSON Test/Poti_NodeMCU_JSON_Generieren/Poti_NodeMCU_JSON_Generieren.ino
@@ -0,0 +1,32 @@
+/*
+Project:  Poti am NodeMCU ESP8266 - JSON senden
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 28.02.2022
+Version:  V1.0
+*/
+
+#include <ArduinoJson.h>
+int potpin = 0;   //Analog pin A0 für das Potenziometer
+StaticJsonDocument<50> doc;
+String meinJson; 
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  meinJson = "";
+  if (analogRead(potpin)<=500) {
+    doc["ledRot"] = true;
+    doc["ledGruen"] = false;
+  }
+  else {
+    doc["ledRot"] = false;
+    doc["ledGruen"] = true;
+  }
+  
+  doc["poti"] = analogRead(potpin);
+  serializeJson(doc, meinJson);
+  Serial.println(meinJson);
+  delay(500);
+}
