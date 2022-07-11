1e306e7c8dfb36672ce061287feaec7629f2d3ce
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2022-07-11 16:11:48 +0200

    Create Projekt02_FastLED_Blaues_Lauflicht.ino

---
+++ FastLED/Projekt02_FastLED_Blaues_Lauflicht/Projekt02_FastLED_Blaues_Lauflicht.ino
@@ -0,0 +1,20 @@
+#include <FastLED.h>
+
+#define NUM_LEDS  60
+#define LED_PIN   4
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(255);
+}
+
+void loop() {
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+    leds[dot] = CRGB::Blue;
+    FastLED.show();
+    leds[dot] = CRGB::Black;
+    delay(50);
+  }
+}
