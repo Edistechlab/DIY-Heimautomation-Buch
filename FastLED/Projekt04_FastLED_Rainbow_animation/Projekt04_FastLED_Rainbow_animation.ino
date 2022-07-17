d6de301c5432f3416f5aa50b74c1448907a10f02
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2022-07-17 10:05:02 +0200

    Create Projekt04_FastLED_Rainbow_animation.ino

---
+++ FastLED/Projekt04_FastLED_Rainbow_animation/Projekt04_FastLED_Rainbow_animation.ino
@@ -0,0 +1,18 @@
+#include <FastLED.h>
+
+#define NUM_LEDS  60
+#define LED_PIN   4
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+}
+
+void loop () {
+  uint16_t beatA = beatsin16(30, 0, 255);
+  uint16_t beatB = beatsin16(20, 0, 255);
+  fill_rainbow(leds, NUM_LEDS, (beatA+beatB)/2, 8);
+  FastLED.show();
+}
