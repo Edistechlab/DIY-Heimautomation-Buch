a0bd47b3f238c02d371c68fe4a9e1564e3f0c644
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2024-03-16 12:56:21 +0100

    Create ESPNNOW_Empfaenger.ino

---
+++ Protokolle/ESPNNOW_Empfaenger/ESPNNOW_Empfaenger.ino
@@ -0,0 +1,52 @@
+/*
+Project:  ESP-NOW Empfänger
+Author:   Thomas Edlinger for www.edistechlab.com
+Date:     Created 06.03.2024
+Version:  V1.0
+IDE:      Arduino IDE 2.3.2
+ 
+Required Board (Tools -> Board -> Boards Manager...)
+ - Board: Arduino ESP32 Boards - by Arduino   V2.0.13
+
+Required libraries (sketch -> include library -> manage libraries)
+ - 
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+const byte LEDPin = 2; //D2
+
+byte address = 0x00;
+String incomingString;
+
+// Define data structure
+typedef struct struct_message {
+  byte a;
+} struct_message;
+
+struct_message myData;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode (LEDPin, OUTPUT);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Button: ");
+  Serial.println(myData.a);
+}
+
+void loop() {
+  if (myData.a == 0) {digitalWrite(LEDPin, LOW);}
+  if (myData.a == 1) {digitalWrite(LEDPin, HIGH);}
+}
