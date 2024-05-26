e7a3add220f2ec511e3a9fddb92fce2fd363063e
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2024-05-26 13:22:03 +0200

    Create secrets.h

---
+++ Projekte/WaterTank/secrets.h
@@ -0,0 +1,9 @@
+// Wi-Fi Settings
+const char *wifi_ssid = "xxx";
+const char *wifi_password = "xxx";
+
+// MQTT Settings
+const char *mqtt_broker = "192.168.0.xxx";
+const char *mqtt_user = "xxx";
+const char *mqtt_password = "xxx";
+const int mqtt_port = 1883;
