c57adcc1fc345e7c23ae9baf8088a09de80962c3
Edis Techlab <53659094+Edistechlab@users.noreply.github.com>
2026-02-24 16:06:29 +0700

    Create secrets.h

---
+++ Projekte/Gas_Cylinder_Weight_Monitor/secrets.h
@@ -0,0 +1,18 @@
+// Wi-Fi Settings
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Einstellungen
+const char *wifi_ssid = "YOUR_WIFI";
+const char *wifi_password = "WIFI-PW";
+
+// MQTT Einstellungen
+const char *mqtt_broker = "192.168.x.x";
+const char *mqtt_user = "MQTT_User";
+const char *mqtt_password = "MQTT_User_PW";
+const int mqtt_port = 1883;
+
+// OTA Passwort
+const char *ota_password = "OTA_PW";
+
+#endif
